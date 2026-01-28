1012cdf3b87ea5158479443d350f41f425429a13
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-01-28 19:42:22 +0530

    Tic-Tac-Toe by using html, css & js

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
-let boxes = document.querySelectorAll(".box")
-let resetbtn = document.querySelector("#reset")
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset");
+let msgContainer = document.querySelector(".container-win")
+let msg = document.querySelector("#msg")
 
 let turnO = true;
 
-const winPatterns =[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
-]; 
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
+const resetGame = ()=>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide")
 
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{ 
-        if (turnO === true) {
-            box.innerText="O";
-            turnO =false;
-        }else{
-            box.innerText="X";
-            turnO = true;
-        }
+}
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO === true) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+const disableBoxes =()=>{
+    for(let box of boxes){
         box.disabled = true;
-        checkWinner();
-    })
-})
-const checkWinner= ()=>{
-    for(let pattern of winPatterns){
-        console.log(pattern[0],pattern[1],pattern[2])
-        console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]])
-        
+    }
+}
+const enableBoxes =()=>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
     }
 }
 
+const showWinner =(winner)=>{
+    msg.innerText =`Congrate, Winner is ${winner}`
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
 
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText; 
+    let pos3Val = boxes[pattern[2]].innerText;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-resetbtn.addEventListener("click",()=>{
-    
-})
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if(pos1Val === pos2Val && pos2Val === pos3Val){
+            showWinner(pos1Val);
+        }
+    }
+  }
+};
+resetbtn.addEventListener("click", resetGame);
